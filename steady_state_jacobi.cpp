5965021cb51eb957a364f981c2a733ac1aa085d0
Surya Vamshi Penumarthi <surya.penumarthi@tum.de>
2021-11-23 08:24:18 +0100

    Function file for Steady State Jacobian Method

---
+++ steady_state_jacobi.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+std::vector<std::vector<double>> steady_state_jacobi(const int& nx,const int& ny,const std::vector<double>& x,const std::vector<double>& y,const double& dx,
+                                                        const double& dy,std::vector<std::vector<double>> Temp,const double& tol,const double& k)
+{
+    std::vector<std::vector<double>> Temp_new = Temp;
+    int iter{1};
+    double err{1};
+
+    while (err>tol)
+    {
+        for (size_t i = 1; i < nx-1; i++)
+        {
+            for (size_t j = 1; j < ny-1; j++)
+            {
+                Temp_new[i][j] = (Temp[i-1][j]+Temp[i+1][j])/(k*dx*dx)+(Temp[i][j-1]+Temp[i][j+1])/(k*dy*dy);
+            }
+        }
+        err = 0;
+        for (size_t i = 1; i < nx-1; i++)
+        {
+            for (size_t j = 1; j < ny-1; j++)
+            {
+                if(err<fabs(Temp_new[i][j]-Temp[i][j])){
+                    err = fabs(Temp_new[i][j]-Temp[i][j]);
+                }
+            }
+        }
+        Temp = Temp_new;
+        iter++;
+    }
+    std::cout<<"Number of iterations took to converge for Steady State Jacobian: "<< iter<< std::endl;
+    return Temp_new;
+}
