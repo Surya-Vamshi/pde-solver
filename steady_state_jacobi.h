a7c774e370e46f2d1218d7852b846a8b55262c06
Surya Vamshi Penumarthi <surya.penumarthi@tum.de>
2021-11-23 08:24:57 +0100

    Header File of Steady State Jacobian Method

---
+++ steady_state_jacobi.h
@@ -0,0 +1,10 @@
+#ifndef STEADY_STATE_JACOBI_H
+#define STEADY_STATE_JACOBI_H
+#include<vector>
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+std::vector<std::vector<double>> steady_state_jacobi(const int&,const int&,const std::vector<double>&,const std::vector<double>&,
+                                                        const double&,const double& ,std::vector<std::vector<double>> ,const double&,const double&);
+
+#endif
