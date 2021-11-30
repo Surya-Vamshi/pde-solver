eadcfa1c89f93ef40a5b5ab65c212afc7ecf7e8f
Surya Vamshi Penumarthi <surya.penumarthi@tum.de>
2021-11-30 19:31:02 +0100

    Added Successive Over Relaxation Method

---
+++ steady_state_successive_over_relaxation.cpp
@@ -0,0 +1,37 @@
+#include"steady_state_successive_over_relaxation.h"
+
+void steady_state_successive_over_relaxation(std::vector<std::vector<double>>& Temp, const int& nx,const int& ny,
+                            const double& dx,const double& dy,const double& tol,const double& k)
+{
+    double rf = 1.1; // Over Relaxation Factor
+    std::vector<std::vector<double>> Temp_new = Temp;
+    int iter{1};
+    double err{1};
+    double term1{0.0}, term2{0.0};
+
+    while (err>tol)
+    {
+        for (size_t i = 1; i < nx-1; i++)
+        {
+            for (size_t j = 1; j < ny-1; j++)
+            {
+                term1 = (Temp_new[i-1][j]+Temp[i+1][j])/(k*dx*dx);
+                term2 = (Temp_new[i][j-1]+Temp[i][j+1])/(k*dy*dy);
+                Temp_new[i][j] = Temp[i][j]*(1-rf) + rf*(term1 + term2);
+            }
+        }
+        err = 0;
+        for (size_t i = 1; i < nx-1; i++)
+        {
+            for (size_t j = 1; j < ny-1; j++)
+            {
+                if(err<fabs(Temp_new[i][j]-Temp[i][j])){
+                    err = fabs(Temp_new[i][j]-Temp[i][j]);
+                }
+            }
+        }
+        Temp = Temp_new;
+        iter++;
+    }
+    std::cout<<"Number of iterations took to converge for Steady State Successive Over Relaxation: "<< iter<< std::endl;
+}
---
+++ steady_state_successive_over_relaxation.h
@@ -0,0 +1,10 @@
+#ifndef STEADY_STATE_SUCCESSIVE_OVER_RELAXATION_H
+#define STEADY_STATE_SUCCESSIVE_OVER_RELAXATION_H
+#include<vector>
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+void steady_state_successive_over_relaxation(std::vector<std::vector<double>>&, const int&,const int&,
+                            const double&,const double& ,const double&,const double&);
+
+#endif
